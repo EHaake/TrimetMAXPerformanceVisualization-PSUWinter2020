4cac71740ee88393d8ac08834a3df44f1cc5b84c
Erik Haake <erik@erikhaakephoto.com>
2020-03-11 22:37:02 -0700

    Improve avg arrival delta graph

--- arrivalTimeDeltaBanfield.js
+++ arrivalTimeDeltaBanfield.js
@@ -31,16 +31,30 @@ function render(data) {
   svg.selectAll(".bar")
      .data(data)
      .enter().append("rect")
-     .attr("class", function(d) { return "bar bar--" + (d.arrival_delta < 0 ? "negative" : "positive"); })
-     .attr("x", function(d) { return x(Math.min(0, d.arrival_delta)); })
-     .attr("y", function(d) { return y(d.stop_name); })
-     .attr("width", function(d) { return Math.abs(x(d.arrival_delta) - x(0)); })
+     .attr("class", d => "bar bar--" + (d.arrival_delta < 0 ? "negative" : "positive"))
+     .attr("x", d => x(Math.min(0, d.arrival_delta)))
+     .attr("y", d => y(d.stop_name))
+     .attr("width", d => Math.abs(x(d.arrival_delta) - x(0)))
      .attr("height", y.bandwidth());
+
+  // Title
+  svg.append('text')
+     .attr('y', -30)
+     .attr('x', width / 4)
+     .attr('class', 'graph-title')
+     .text('Average Arrival Delta (Banfield)');
 
   svg.append("g")
      .attr("class", "x axis")
      .attr("transform", "translate(0," + height + ")")
      .call(xAxis);
+
+  // X-Axis label
+  svg.append('text')
+     .attr('y', height + (height / 8))
+     .attr('x', width / 2 - (width / 10))
+     .attr('class', 'x-axis-label')
+     .text('Seconds Late');
 
   svg.append("g")
      .attr("class", "y axis")
